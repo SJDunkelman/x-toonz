00a7d3ab1024f938ad56890f94f37ba773d050b8
Simon Dunkelman <sd9518@ic.ac.uk>
2022-06-06 11:31:17 +0100

    trying animation

--- src/components/AnimatedBuilding.js
+++ src/components/AnimatedBuilding.js
@@ -6,12 +6,12 @@ export default function AnimatedBuilding(){
     return (
         <>
             <div className="h-screen relative overflow-hidden hidden md:block">
-                <StaticImage src="../images/building/building-full.png" alt="Miami city background" className={`${imageClasses} z-10`} />
-                {/*<StaticImage src="../images/building/cloud-1.png" alt="Miami city background" className={`${imageClasses} z-20 clouds-1 -translate-x-1/3`} />*/}
-                {/*<StaticImage src="../images/building/cloud-2.png" alt="Miami city background" className={`${imageClasses} z-30 clouds-2 -translate-x-2/3`} />*/}
-                {/*<StaticImage src="../images/building/cloud-3.png" alt="Miami city background" className={`${imageClasses} z-40 clouds-3`} />*/}
-                {/*<StaticImage src="../images/building/strobe-1.png" alt="Miami city background" className={`${imageClasses} z-40 strobe-1`} />*/}
-                {/*<StaticImage src="../images/building/strobe-2.png" alt="Miami city background" className={`${imageClasses} z-50 strobe-2`} />*/}
+                <StaticImage src="../images/building/bg-building.png" alt="Miami city background" className={`${imageClasses} z-10`} />
+                <StaticImage src="../images/building/cloud-1.png" alt="Miami city background" className={`${imageClasses} z-20 clouds-1 -translate-x-1/3`} />
+                <StaticImage src="../images/building/cloud-2.png" alt="Miami city background" className={`${imageClasses} z-30 clouds-2 -translate-x-2/3`} />
+                <StaticImage src="../images/building/cloud-3.png" alt="Miami city background" className={`${imageClasses} z-40 clouds-3`} />
+                <StaticImage src="../images/building/strobe-1.png" alt="Miami city background" className={`${imageClasses} z-40 strobe-1`} />
+                <StaticImage src="../images/building/strobe-2.png" alt="Miami city background" className={`${imageClasses} z-50 strobe-2`} />
             </div>
             <div className="h-full flex justify-center items-center md:hidden">
                 <StaticImage src="../images/building/half-building-complete.png" alt="Miami city background" className="w-screen" />
